bf631dbd71135ab828d7da879cf739a4623a77fe
Daisuke-Tsuji <kixrsb24@gmail.com>
2020-07-29 21:47:58 +0900

    Initial Commit

---
+++ config/application.rb
@@ -0,0 +1,29 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module SubTaskApp
+  class Application < Rails::Application
+    config.i18n.default_locale = :ja
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 6.0
+
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
+    config.action_view.embed_authenticity_token_in_remote_forms = true
+    config.generators do |g|
+      g.test_framework :rspec,
+            view_specs: false,
+            helper_specs: false,
+            controller_specs: false,
+            routing_specs: false
+    end
+
+  end
+end
